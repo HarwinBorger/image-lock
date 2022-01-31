36108ca0400b8c50e613bdf7926e304a3312c13c
Harwin Borger <harwin@studioborger.nl>
2022-02-01 00:47:11 +0100

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+
+var root = "./images";
+
+console.clear();
+let lock = [];
+if (fs.existsSync('image-lock.json')) {
+  lock = fs.readFileSync('image-lock.json');
+  lock = JSON.parse(lock);
+}
+
+const imageLock = [];
+
+loopFolder(root).then(() => {
+  console.log(imageLock)
+
+  fs.writeFile('./image-lock.json', JSON.stringify(imageLock, null, 2), err => {
+    if (err) {
+      console.error(err)
+    }
+    //file written successfully
+  })
+});
+
+async function loopFolder(input, lvl = 1) {
+  var indent = '|'.repeat(lvl);
+  console.log(chalk.blue(`${indent} Open folder:`), input);
+
+  const files = await fs.promises.readdir(input);
+  let promises = files.map(async file => {
+    const filePath = path.join(input, file)
+    const stat = await fs.promises.stat(filePath);
+
+    if (stat.isFile()) {
+      console.log(chalk.yellow(`${indent}- Found file:`), filePath, stat.ctime);
+
+      if (!exists(filePath).length) {
+        console.log(chalk.magenta('new file found'), filePath);
+      }else{
+        console.log(chalk.red('already exists'), filePath);
+      }
+
+      imageLock.push({'path': filePath, 'ctime': stat.ctime});
+    } else if (stat.isDirectory()) {
+      await loopFolder(filePath, lvl + 1);
+    }
+
+    return filePath;
+  })
+
+  return await Promise.all(promises).then((files) => {
+    console.log(chalk.green(`${indent} Done folder:`), input);
+  });
+}
+
+function exists(input) {
+  return lock.filter(function (item) {
+    return item.path === input;
+  });
+}
