8606b83434e3c5b8945526e063b2d8b3730f546f
Harwin Borger <harwin.borger@athom.nl>
2022-03-01 17:39:26 +0100

    wip

--- test.js
+++ test.js
@@ -2,5 +2,7 @@
 
 import {ImageLock} from "./index.mjs";
 
-const imageLock = new ImageLock({path: './images', action: 'webp', debug: false});
+const imageLock = new ImageLock({path: './images', action: '1235', debug: true});
+imageLock.addAction('wefefafeabp',(path)=>{console.log(path)});
 imageLock.run();
+
